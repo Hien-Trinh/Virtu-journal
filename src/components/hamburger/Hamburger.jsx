669e3da4199ba92c5b49ab51e259e51de1c56e89
Hien-Minh <htrinh@macalester.edu>
2024-03-20 10:23:12 -0500

    Fixed charterPage

--- src/components/hamburger/Hamburger.jsx
+++ src/components/hamburger/Hamburger.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Link from "next/link"
-import styles from "./Hamburger.module.css"
+import styles from "./hamburger.module.css"
 import { useState } from "react"
 
 const Hamburger = () => {
