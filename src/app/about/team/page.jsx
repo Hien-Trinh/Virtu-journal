9e43b4e71e2528a191a416f71a818ae31cd8bb39
Hien-Minh <htrinh@macalester.edu>
2024-03-25 14:39:52 -0500

    Updated About pages sideMenu to be responsive, added id attribute to member objects.

--- src/app/about/team/page.jsx
+++ src/app/about/team/page.jsx
@@ -7,72 +7,84 @@ import SideMenu from "@/components/aboutComponents/sideMenu/SideMenu"
 const TeamPage = () => {
     const team = [
         {
+            _id: 1,
             name: "Walter Moore",
             role: "Founder & Content Editor",
             major: "International Studies",
             year: "First-Year",
         },
         {
+            _id: 2,
             name: "Khant Wai Yan",
             role: "Founder & Managerial Editor",
             major: "International Studies",
             year: "Junior",
         },
         {
+            _id: 3,
             name: "Dean Hu",
             role: "Content Writer",
             major: "International Studies",
             year: "Sophomore",
         },
         {
+            _id: 4,
             name: "Katie Whelan",
             role: "Assistant Editor",
             major: "International Studies",
             year: "First-year",
         },
         {
+            _id: 5,
             name: "Sydney Stratton",
             role: "Assistant Editor",
             major: "International Studies",
             year: "First-year",
         },
         {
+            _id: 6,
             name: "Cole Conklin",
             role: "Secretary",
             major: "International Studies",
             year: "First-year",
         },
         {
+            _id: 7,
             name: "Colette Lawler",
             role: "Assistant Editor",
             major: "Undecided",
             year: "First-year",
         },
         {
+            _id: 8,
             name: "Nini Yiu",
             role: "Associate Editor",
             major: "English Literature",
             year: "Senior",
         },
         {
+            _id: 9,
             name: "David Trinh",
             role: "Technologist & Walter's Roommate",
             major: "Computer Science",
             year: "First-year",
         },
         {
+            _id: 10,
             name: "Nanami Chesen",
             role: "Outreach Coordinator",
             major: "International Studies",
             year: "Sophomore",
         },
         {
+            _id: 11,
             name: "Adam Rose",
             role: "Content Writer",
             major: "English Literature",
             year: "Senior",
         },
         {
+            _id: 12,
             name: "Yosephine Manihuruk",
             role: "Associate Editor",
             major: "International Studies",
@@ -88,7 +100,7 @@ const TeamPage = () => {
                     <h1 className={styles.title}>Meet the Team</h1>
                     <div className={styles.teamList}>
                         {team?.map((member) => (
-                            <div>
+                            <div key={member._id}>
                                 <div className={styles.detail}>
                                     <span className={styles.major}>
                                         {member.major} -{" "}
